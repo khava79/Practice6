ccb76383ee51743ff97600415b5ba8c6bc4c2d97
khava79 <havatsechoeva011@gmail.com>
2026-01-11 23:19:32 +0300

    Update CheckedExceptionTask3.java

--- src/main/java/practice7/CheckedExceptionTask3.java
+++ src/main/java/practice7/CheckedExceptionTask3.java
@@ -2,7 +2,7 @@ package practice7;
 
 public class CheckedExceptionTask3 extends Exception {
     public CheckedExceptionTask3 (String message) {
-        super();
+        super(message);
     }
 
 }
