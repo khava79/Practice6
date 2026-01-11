552ad4ef072de70c4dc8927bd5453722604bc3d4
khava79 <havatsechoeva011@gmail.com>
2026-01-11 23:19:52 +0300

    Update UncheckedExceptionTask4.java

--- src/main/java/practice7/UncheckedExceptionTask4.java
+++ src/main/java/practice7/UncheckedExceptionTask4.java
@@ -1,8 +1,8 @@
-package practice7;
+spackage practice7;
 import java.util.regex.Pattern;
 
 public class UncheckedExceptionTask4 extends RuntimeException {
     public UncheckedExceptionTask4(String message) {
-        super();
+        super(message);
     }
 }
