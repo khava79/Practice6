fee0a1c12641c1826f6b127d57a065bfc0adb0ef
khava79 <havatsechoeva011@gmail.com>
2026-02-06 13:37:44 +0300

    Update Main3.java

--- src/main/java/practice8/StreamAPIAgr/Main3.java
+++ src/main/java/practice8/StreamAPIAgr/Main3.java
@@ -7,7 +7,7 @@ public class Main3 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(9, 4, 76, 823);
         int sumOfAllNumbers = numbers.stream()
-                .mapToInt(n -> n)
+                .mapToInt(Integer::intValue)
                 .sum();
         System.out.println(sumOfAllNumbers);
     }
